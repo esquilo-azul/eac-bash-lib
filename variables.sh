3a639190cb4ebee1d72022b575e36354283877ff
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-10-10 16:49:45 -0300

    var_blank_r().

--- variables.sh
+++ variables.sh
@@ -1,6 +1,14 @@
 set -u
 set -e
 
+function var_blank_r() {
+  if var_present_r "$1"; then
+    return 1
+  else
+    return 0
+  fi
+}
+
 function var_present_r() {
   NAME="$1"
   if [ -v "${NAME}" ]; then
