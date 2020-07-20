32159f3a791f01fa64e896c2f1348b464a399a02
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-07-19 23:00:07 -0300

    ruby_gem_installed: fix.

--- ruby/gem.sh
+++ ruby/gem.sh
@@ -1,6 +1,6 @@
 function ruby_gem_installed() {
   for GEM in "$@"; do
-    if gem list --local | grep -io '^[0-9a-z\-]\+' | grep -i "^$GEM\$" &> /dev/null; then
+    if ! gem list --local | grep -io '^[0-9a-z\-]\+' | grep -i "^$GEM\$" &> /dev/null; then
       return 1
     fi
   done
