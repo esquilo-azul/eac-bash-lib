225ceadf2a20e980e42dfedb150283e916c1c5a9
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-11-12 13:05:08 -0300

    sudoers_write().

---
+++ sudo.sh
@@ -0,0 +1,16 @@
+set -u
+set -e
+
+function sudoers_write() {
+  if [ $# -lt 1 ]; then
+    error "Usage: ${FUNCNAME[0]} <TARGET_FILE> [<SOURCE_FILE>='-']\n"
+    return 1
+  fi
+
+  local TARGET_FILE="$1"
+  local SOURCE_FILE="$(cli_arg 2 '-' "$@")"
+  SOURCE_FILE="$(cli_file_path_or_stdin "$SOURCE_FILE")"
+
+  sudo EDITOR='tee' visudo -f "$TARGET_FILE" < "$SOURCE_FILE"
+  sudo chmod 440 "$TARGET_FILE"
+}
