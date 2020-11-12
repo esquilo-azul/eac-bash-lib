be835a62763db8745093b59d1fba6ea0cd32089d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-11-12 13:28:33 -0300

    cli_file_path_or_stdout().

--- cli.sh
+++ cli.sh
@@ -8,3 +8,15 @@ function cli_file_path_or_stdin() {
   fi
   printf "%s\n" "$SOURCE_PATH"
 }
+
+function cli_file_path_or_stdout() {
+  local TARGET_PATH="$1"
+  if [ "$TARGET_PATH" = '-' ]; then
+    local TARGET_PATH='/dev/stdout'
+    if [ ! -f "$TARGET_PATH" ]; then
+      fatal_error "File \"$TARGET_PATH\" does not exist or is not a file (${FUNCNAME[@]})\n"
+      return 1
+    fi
+  fi
+  printf "%s\n" "$TARGET_PATH"
+}
