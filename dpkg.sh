85dd9da0390a229d10fdaa80c78954131a7f19fc
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-12-04 13:59:44 +0000

    dpkg_file_installed().

--- dpkg.sh
+++ dpkg.sh
@@ -1,6 +1,16 @@
 set -u
 set -e
 
+function dpkg_file_installed() {
+  for DEB_FILE in $@; do
+    PACKAGE_NAME="$(dpkg --field "$DEB_FILE" Package)"
+    if ! deb_installed "$PACKAGE_NAME"; then
+      return 1
+    fi
+  done
+}
+export -f dpkg_file_installed
+
 function dpkg_installed() {
   for PKG in $@; do
     RESULT=`dpkg-query -W '-f=${Status}' "$PKG"`
