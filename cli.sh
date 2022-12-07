63c5a3433bfe9e38fac2327b30a63b6021204a36
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-12-07 01:55:52 -0300

    cli_file_path_or_default(): extract from cli_file_path_or_stdin().

--- cli.sh
+++ cli.sh
@@ -14,15 +14,22 @@ function cli_arg() {
 }
 export -f cli_arg
 
-function cli_file_path_or_stdin() {
-  local SOURCE_PATH="$1"
-  if [ "$SOURCE_PATH" = '-' ]; then
-    local SOURCE_PATH='/dev/stdin'
-  elif [ ! -f "$SOURCE_PATH" ]; then
-    fatal_error "File \"$SOURCE_PATH\" does not exist or is not a file (${FUNCNAME[@]})\n"
+
+function cli_file_path_or_default() {
+  local RESULT="$1"
+  local DEFAULT_RESULT="$2"
+  if [ "$RESULT" = '-' ]; then
+    local RESULT="$DEFAULT_RESULT"
+  elif [ ! -f "$RESULT" ]; then
+    fatal_error "File \"$RESULT\" does not exist or is not a file (${FUNCNAME[@]})\n"
     return 1
   fi
-  printf "%s\n" "$SOURCE_PATH"
+  printf "%s\n" "$RESULT"
+}
+export -f cli_file_path_or_default
+
+function cli_file_path_or_stdin() {
+  cli_file_path_or_default "$1" '/dev/stdin'
 }
 export -f cli_file_path_or_stdin
 
