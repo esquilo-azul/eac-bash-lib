4f4170e8e22ad4ec0f4aec4e2ff9b1fdd6b7e83c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-12-11 13:38:03 -0300

    python_installed_single(): extract from "python_installed".

--- python.sh
+++ python.sh
@@ -15,15 +15,24 @@ function python_assert_installed() {
 }
 export -f python_assert_installed
 
+# Deprecated: use "package_installed python" instead.
 function python_installed() {
   for PKG in $@; do
-    RESULT="$("$(python_pip_command)" list | cut -d ' ' -f1 | grep "$PKG")"
-    if [ "$RESULT" != "$PKG" ] ; then
+    if ! python_installed_single "$PKG"; then
       return 1
     fi
   done
 }
 export -f python_installed
+
+function python_installed_single() {
+  PACKAGE="$1"
+  RESULT="$("$(python_pip_command)" list | cut -d ' ' -f1 | grep "$PACKAGE")"
+  if [ "$RESULT" != "$PACKAGE" ] ; then
+    return 1
+  fi
+}
+export -f python_installed_single
 
 function python_pip_command() {
   if var_present_r 'PIP_COMMAND'; then
