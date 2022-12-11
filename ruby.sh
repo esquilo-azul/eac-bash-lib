6d488a2fabc82f743c1cae3e20332206b759857c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-12-11 13:24:55 -0300

    ruby_installed_single(): extract from ruby_gem_installed().

--- ruby.sh
+++ ruby.sh
@@ -5,3 +5,8 @@ function ruby_install_multiple() {
   gem install -V "$@"
 }
 export -f ruby_install_multiple
+
+function ruby_installed_single() {
+  gem list --local | grep -io '^[0-9a-z\-]\+' | grep -i "^${1}\$" &> /dev/null
+}
+export -f ruby_installed_single
