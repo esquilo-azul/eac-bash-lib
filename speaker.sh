0c99a35b7cf9e0aa8a15fb34886ca58c2fc17e1f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2022-12-12 11:21:42 -0300

    speaker.sh: export colors' variables.

--- speaker.sh
+++ speaker.sh
@@ -1,11 +1,11 @@
 set -u
 set -e
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;93m'
-CYAN='\033[0;36m'
-NC='\033[0m'
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;93m'
+export CYAN='\033[0;36m'
+export NC='\033[0m'
 
 function outerr() {
   local first=1
