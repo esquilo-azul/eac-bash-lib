bb927d9f81c8655a4c766d0be907f6448694dd1e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-04-29 01:38:52 -0300

    symlink.sh: extract from "path.sh".

--- path.sh
+++ path.sh
@@ -17,28 +17,3 @@ function path_expand() {
   fi
 }
 export -f path_expand
-
-function symlink_assert() {
-  infom 'Asserting symlink...'
-  local SOURCE="$1"
-  local TARGET="$2"
-  infov_compact 'SOURCE' 'TARGET'
-  if [ -L "$TARGET" ]; then
-    if [ "$(realpath "$TARGET")" == "$SOURCE" ]; then
-      infom 'Symlink already exist with the correct source.'
-      return 0
-    else
-      infom 'Target does not point to the source. Removing target...'
-      rm "$TARGET"
-    fi
-  elif [ -e "$TARGET" ]; then
-    error 'Target already exists, is not a symlink and because of this cannot be removed.'
-    return 1
-  else
-    infom "Target does not exist."
-  fi
-  infom 'Creating symlink...'
-  mkdir -p "$(dirname "$TARGET")"
-  ln -s "$SOURCE" "$TARGET"
-}
-export -f symlink_assert
