576874bcf69cd91c54bdfa1baed65ddeeef1d631
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-04-29 19:31:47 -0300

    symlink_assert(): execute commands with sudo_run().

--- symlink.sh
+++ symlink.sh
@@ -3,12 +3,12 @@ function symlink_assert() {
   local LINK_PATH="$1"
   local TARGET_PATH="$2"
   if [ -L "$LINK_PATH" ]; then
-    if [ "$(realpath "$LINK_PATH")" == "$TARGET_PATH" ]; then
+    if [ "$(sudo_run realpath "$LINKPATH")" == "$TARGET" ]; then
       infom 'Symlink already exist with the right target.'
       return 0
     else
       infom 'Symlink does not point to the target. Removing target...'
-      rm "$LINK_PATH"
+      sudo_run rm "$LINK_PATH"
     fi
   elif [ -e "$LINK_PATH" ]; then
     error 'A object in the symlink path already exists, is not a symlink and because of this cannot be removed.'
@@ -17,7 +17,7 @@ function symlink_assert() {
     infom "Symlink does not exist."
   fi
   infom 'Creating symlink...'
-  mkdir -p "$(dirname "$LINK_PATH")"
-  ln -s "$TARGET_PATH" "$LINK_PATH"
+  sudo_run mkdir -p "$(dirname "$LINK_PATH")"
+  sudo_run ln -s "$TARGET_PATH" "$LINK_PATH"
 }
 export -f symlink_assert
