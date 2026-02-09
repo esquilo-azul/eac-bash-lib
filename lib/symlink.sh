b5c1800cb18dcfd7858a76daebe1b9db8f55c038
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-02-09 10:58:51 -0300

    symlink_assert(): messages show paths.

--- lib/symlink.sh
+++ lib/symlink.sh
@@ -4,19 +4,19 @@ function symlink_assert() {
   local TARGET_PATH="$2"
   if symlink_exist "$LINK_PATH"; then
     if symlink_target "$LINK_PATH" "$TARGET_PATH"; then
-      infom 'Symlink already exist with the right target.'
+      infom "Symlink \"${LINK_PATH}\" already exist with the right target \"$TARGET_PATH\"."
       return 0
     else
-      infom 'Symlink does not point to the target. Removing target...'
+      infom "Symlink \"${LINK_PATH}\" does not point to the target \"$TARGET_PATH\". Removing target..."
       sudo_run rm "$LINK_PATH"
     fi
   elif [ -e "$LINK_PATH" ]; then
-    error 'A object in the symlink path already exists, is not a symlink and because of this cannot be removed.'
+    error "A object in the symlink path \"${LINK_PATH}\" already exists, is not a symlink and because of this cannot be removed."
     return 1
   else
-    infom "Symlink does not exist."
+    infom "Symlink \"${LINK_PATH}\" does not exist."
   fi
-  infom 'Creating symlink...'
+  infom "Creating symlink  \"${LINK_PATH}\" => \"$TARGET_PATH\"..."
   sudo_run mkdir -p "$(dirname "$LINK_PATH")"
   sudo_run ln -s "$TARGET_PATH" "$LINK_PATH"
 }
