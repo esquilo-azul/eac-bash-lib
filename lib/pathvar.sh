8c99b158269e771afc244a2e28be309d7ab2d027
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-08-20 15:57:28 -0300

    pathvar_find_files().

--- lib/pathvar.sh
+++ lib/pathvar.sh
@@ -8,6 +8,17 @@ function pathvar_assert() {
 }
 export -f pathvar_assert
 
+function pathvar_find_files() {
+  local PATHVAR="$1"
+  local FILE_SUBPATH_PATTERN="$2"
+
+  while read -r NODE; do
+    [ -d "$NODE" ] || continue
+    find "${NODE}" -type f -path "${NODE}/${FILE_SUBPATH_PATTERN}" 2>/dev/null
+  done < <(pathvar_to_lines "${PATHVAR}")
+}
+export -f pathvar_find_files
+
 function pathvar_join() {
   local ACUM=''
   for VALUE in "$@"; do
